9d9597a86e345d3ae2fd0a8aab17b54dcc57b717
jrangelhayward <jrangel@hayward.com>
2026-07-21 12:47:30 -0400

    Fix invalid inline style, apply strong css overrides for h2 and p elements in Magento

--- src/App.js
+++ src/App.js
@@ -296,7 +296,7 @@ const App = () => {
       <>
         <header id="cross-reference-header" className={`banner ${isBannerShrink ? 'banner-shrink' : ''}`}>
           <div className="banner-content">
-            <h2 className="text-white" style={{color: '#ffffff !important'}}>Cross-Reference</h2>
+            <h2 className="text-white">Cross-Reference</h2>
             {activeTab === 'home' ? (
               <p className="text-white">
                 Replacing a piece of equipment by another brand? <br></br>
